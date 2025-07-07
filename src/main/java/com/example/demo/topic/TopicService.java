e4a9762f02fa8cd656e2c28dc149d9ee9bff4d03
tray0019 <tray0019@algonquinlive.com>
2025-07-07 13:39:02 -0400

    In TopicService class, I have added method for getting id, adding topic, updating and deleting. Alsoa List method that returns all Topic.

--- src/main/java/com/example/demo/topic/TopicService.java
+++ src/main/java/com/example/demo/topic/TopicService.java
@@ -5,6 +5,10 @@
  */
 package com.example.demo.topic;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 /*
@@ -14,4 +18,42 @@ import org.springframework.stereotype.Service;
 @Service
 public class TopicService {
 
+	List<Topic> topics = new ArrayList<>(Arrays.asList(
+			new Topic("Spring","Spring Framework","Description"),
+			new Topic("Java","Core Java","Java Description"),
+			new Topic("JavaScript","JS","JS Description")
+			));
+	
+	public List<Topic> getAllTopic(){
+		return topics;
+	}
+	
+	public Topic getTopic(String id) {
+		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();//lambda expression 
+	}
+	
+	public void addTopic(Topic topic) {
+		topics.add(topic);
+	}
+	
+	public void updateTopic(String id, Topic topic) {
+		
+		for(int i = 0; i < topics.size(); i++) {
+			Topic t = topics.get(i);
+			if(t.getId().equals(id)) {
+				topics.set(i, topic);
+				return;
+			}
+		}
+	}
+	
+	public void deleteTopic(String id) {
+		topics.removeIf(t -> t.getId().equals(id));
+	}
+	
+	
+	
+	
+	
+	
 }
