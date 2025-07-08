cfee98c298daf425c56a4c522bdc44470ba51e56
tray0019 <tray0019@algonquinlive.com>
2025-07-08 14:02:17 -0400

    Updated TopicService method using topicRepository methods.

--- README.md
+++ README.md
@@ -24,6 +24,7 @@ Result: RESTful API tested on browser and Postman and both succesfully worked!
 8. Added JPA annotation for my Topic class
 9. Added TopicRepository interface that extends JpaRepository
 10. In my TopicService I have added my TopicRepository instance field with @Autowired annotations
+11. Updated TopicService method using topicRepositroy method. 
 
 
 ## Learned
--- src/main/java/com/example/demo/topic/TopicService.java
+++ src/main/java/com/example/demo/topic/TopicService.java
@@ -8,6 +8,7 @@ package com.example.demo.topic;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -28,31 +29,30 @@ public class TopicService {
 			new Topic("JavaScript","JS","JavaScript Description")
 			));
 	
+	/*
+	 * Find all Topic
+	 */
 	public List<Topic> getAllTopic(){
-		return topics;
+		return topicRepository.findAll();
 	}
 	
-	public Topic getTopic(String id) {
-		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();//lambda expression 
+	/*
+	 * Get topic by id
+	 */
+	public Optional<Topic> getTopic(String id) {
+		return topicRepository.findById(id);
 	}
 	
 	public void addTopic(Topic topic) {
-		topics.add(topic);
+		topicRepository.save(topic);
 	}
 	
 	public void updateTopic(String id, Topic topic) {
-		
-		for(int i = 0; i < topics.size(); i++) {
-			Topic t = topics.get(i);
-			if(t.getId().equals(id)) {
-				topics.set(i, topic);
-				return;
-			}
-		}
+		topicRepository.save(topic);
 	}
 	
 	public void deleteTopic(String id) {
-		topics.removeIf(t -> t.getId().equals(id));
+		topicRepository.deleteById(id);
 	}
 	
 	
